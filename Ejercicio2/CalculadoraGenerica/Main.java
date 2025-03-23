53654b9eb15d63c54678eb252d3aeb5b89bbc237
Janitza Tapia <janitzat4@gmail.com>
2025-03-23 14:05:14 -0500

    Codigo con comentarios

--- Ejercicio2/CalculadoraGenerica/Main.java
+++ Ejercicio2/CalculadoraGenerica/Main.java
@@ -3,6 +3,7 @@ package Ejercicio2.CalculadoraGenerica;
 import java.util.Scanner;
 
 public class Main {
+    //metodos genericos
     static <T extends Number> double suma(T valor1, T valor2){
         return valor1.doubleValue() + valor2.doubleValue();
     }
@@ -30,7 +31,7 @@ public class Main {
     static <T extends Number> double raizCubica(T valor){
         return Math.cbrt(valor.doubleValue());
     }
-
+    //metodo para verificar que solo se ingresen numeros (Integer o Double)
     private static Number leerNumero(Scanner scanner) {
         while (true) {
             if (scanner.hasNextInt()) {
@@ -39,13 +40,13 @@ public class Main {
                 return scanner.nextDouble();
             } else {
                 System.out.println("Error: Ingrese un número válido.");
-                scanner.next();
+                scanner.next(); //para descartar la entrada actual del usuario
             }
         }
     }
-
+    // metodo solo para las opciones:suma,resta,producto,division,potencia. Recibe 2 numeros
     private static void realizarOperacion(int opcion, Operador<Number> operacion) {
-        Number num1 = operacion.getValor1();
+        Number num1 = operacion.getValor1(); // de tipo Number
         Number num2 = operacion.getValor2();
 
         switch (opcion) {
@@ -70,6 +71,8 @@ public class Main {
                 break;
         }
     }
+    
+    // metodo solo para las opciones:raiz cuadrada y raiz cubica. Solo recibe un numero
     private static void realizarRaiz(int opcion, Number num) {
         if (opcion == 6) {
             if (num.doubleValue() < 0) {
@@ -97,32 +100,37 @@ public class Main {
             System.out.println("7. Raíz Cúbica");
             System.out.println("8. Salir");
             System.out.print("Opción: ");
-            
+            //Verificamos que la opcion sea un numero
             if (!scanner.hasNextInt()){
                 System.out.println("Error: debe ingresar una opcion valida");
                 scanner.next();
                 continue;
             }
-
             opcion = scanner.nextInt();
+
+            //opcion salir
             if (opcion == 8) {
                 System.out.println("El programa terminó :P");
                 break;
             }
+
+            //lee el primer num
             System.out.print("Ingrese el primer número: ");
             Number num1 = leerNumero(scanner);
 
-            Number num2 = null;
-            if (opcion != 6 && opcion != 7) { 
+            Number num2 = null; //hacemos null al 2do num de inicio
+            //si son las operaciones de raices no necesitamos el 2do num
+            if (opcion != 6 && opcion != 7) {  //si son otras operaciones si usamos el 2do num
                 System.out.print("Ingrese el segundo número: ");
                 num2 = leerNumero(scanner);
             }
 
-            if (opcion >= 1 && opcion <= 5) {
-                Operador<Number> operacion = new Operador<>(num1, num2);
+            //verificamos que opcion es para llamar a los metodos correspondientes
+            if (opcion >= 1 && opcion <= 5) { //suma,resta,producto,division,potencia
+                Operador<Number> operacion = new Operador<>(num1, num2);//dos parametros
                 realizarOperacion(opcion, operacion);
-            } else if (opcion == 6 || opcion == 7) {
-                realizarRaiz(opcion, num1);
+            } else if (opcion == 6 || opcion == 7) { //raiz cuadrada y cubica
+                realizarRaiz(opcion, num1);//un parametro
             } else {
                 System.out.println("Opción no válida.");
             }
--- Ejercicio2/CalculadoraGenerica/Operador.java
+++ Ejercicio2/CalculadoraGenerica/Operador.java
@@ -1,14 +1,17 @@
 package Ejercicio2.CalculadoraGenerica;
 
-public class Operador<T extends Number>{
+public class Operador<T extends Number>{ 
+    //Number es una clase abstracta que solo recibe datos de tipos numericos primitivos
     private T valor1;
     private T valor2;
-    
+
+    //constructor
     public Operador(T valor1, T valor2){
         this.valor1 = valor1;
         this.valor2 = valor2;
     }
-
+    
+    //getters
     public T getValor1(){
         return valor1;
     }
