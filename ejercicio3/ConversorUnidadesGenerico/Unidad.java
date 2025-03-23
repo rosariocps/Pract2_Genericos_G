1e0e43b175108cc43cd7f4dc0e20d83262aef3d0
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-03-22 20:14:43 -0500

    Unidad<T>

--- ejercicio3/ConversorUnidadesGenerico/Unidad.java
+++ ejercicio3/ConversorUnidadesGenerico/Unidad.java
@@ -1,5 +1,12 @@
 package ejercicio3.ConversorUnidadesGenerico;
 
-public class Unidad {
+public class Unidad<T> {
+    private T valor;
+    private T tipoUnidad;
+
+    public Unidad(T valor, T tipoUnidad){
+        this.valor = valor;
+        this.tipoUnidad = tipoUnidad;
+    }
     
 }
