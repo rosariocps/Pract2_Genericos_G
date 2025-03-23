c1bb14dbd4b95d5bf67f3d94e50f632a55e7d1a4
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-03-22 20:30:28 -0500

    Clase Unidad<T>: Getters y setters

--- ejercicio3/ConversorUnidadesGenerico/Unidad.java
+++ ejercicio3/ConversorUnidadesGenerico/Unidad.java
@@ -4,9 +4,26 @@ public class Unidad<T> {
     private T valor;
     private T tipoUnidad;
 
+    //Contructor
     public Unidad(T valor, T tipoUnidad){
         this.valor = valor;
         this.tipoUnidad = tipoUnidad;
     }
-    
+
+    //Getters
+    public T getValor(){
+        return valor;
+    }
+    public T getTipoUnidad(){
+        return tipoUnidad;
+    }
+
+    //Setters
+    public void setValor(T valor){
+        this.valor = valor;
+    }
+    public void setTipoUnidad(T tipoUnidad){
+        this.tipoUnidad = tipoUnidad;
+    }
+
 }
