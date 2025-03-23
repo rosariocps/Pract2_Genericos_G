7d443df768d58b2478cafbd3d55a523f8c337758
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-03-23 18:52:26 -0500

    Clase Conversor: Terminado de metodo convertir

--- ejercicio3/ConversorUnidadesGenerico/Conversor.java
+++ ejercicio3/ConversorUnidadesGenerico/Conversor.java
@@ -14,9 +14,9 @@ public class Conversor<T extends Number> {
         else if (unidadOrigen.equals("pies") && unidadDestino.equals("metros"))
             resultado = valorNumerico / 3.28084;
         else if (unidadOrigen.equals("pies") && unidadDestino.equals("pulgadas"))
-            resultado = valorNumerico * 12;
+            resultado = valorNumerico * 12.0;
         else if (unidadOrigen.equals("pulgadas") && unidadDestino.equals("pies"))
-            resultado = valorNumerico / 12;
+            resultado = valorNumerico / 12.0;
         else if (unidadOrigen.equals("metros") && unidadDestino.equals("pulgadas"))
             resultado = valorNumerico * 39.3701;
         else if (unidadOrigen.equals("pulgadas") && unidadDestino.equals("metros"))
@@ -27,7 +27,42 @@ public class Conversor<T extends Number> {
             resultado = valorNumerico * 2.20462;
         else if (unidadOrigen.equals("libras") && unidadDestino.equals("kilogramos"))
             resultado = valorNumerico / 2.20462;
+        else if (unidadOrigen.equals("kilogramos") && unidadDestino.equals("onzas"))
+            resultado = valorNumerico * 35.274;        
+        else if (unidadOrigen.equals("onzas") && unidadDestino.equals("kilogramos"))
+            resultado = valorNumerico / 35.274;
+        else if (unidadOrigen.equals("libras") && unidadDestino.equals("onzas"))
+            resultado = valorNumerico * 16.0;
+        else if (unidadOrigen.equals("onzas") && unidadDestino.equals("libras"))
+            resultado = valorNumerico / 16.0;
+        
+        // Tiempo
+        else if (unidadOrigen.equals("segundos") && unidadDestino.equals("minutos"))
+            resultado = valorNumerico / 60.0;
+        else if (unidadOrigen.equals("minutos") && unidadDestino.equals("segundos"))
+            resultado = valorNumerico * 60.0;
+        else if (unidadOrigen.equals("segundos") && unidadDestino.equals("horas"))
+            resultado = valorNumerico / 3600;
+        else if (unidadOrigen.equals("horas") && unidadDestino.equals("segundos"))
+            resultado = valorNumerico * 3600;
+        else if (unidadOrigen.equals("minutos") && unidadDestino.equals("horas"))
+            resultado = valorNumerico / 60.0;
+        else if (unidadOrigen.equals("horas") && unidadDestino.equals("minutos"))
+            resultado = valorNumerico * 60.0;
 
+        // Temperatura
+        else if (unidadOrigen.equals("celsius") && unidadDestino.equals("fahrenheit"))
+            resultado = (valorNumerico * 9 / 5) + 32;
+        else if (unidadOrigen.equals("fahrenheit") && unidadDestino.equals("celsius"))
+            resultado = (valorNumerico - 32) * 5 / 9;
+        else if (unidadOrigen.equals("celsius") && unidadDestino.equals("kelvin"))
+            resultado = valorNumerico + 273.15;
+        else if (unidadOrigen.equals("kelvin") && unidadDestino.equals("celsius"))
+            resultado = valorNumerico - 273.15;
+        else if (unidadOrigen.equals("fahrenheit") && unidadDestino.equals("kelvin"))
+            resultado = ((valorNumerico - 32) * 5 / 9) + 273.15;
+        else if (unidadOrigen.equals("kelvin") && unidadDestino.equals("fahrenheit"))
+            resultado = ((valorNumerico - 273.15) * 9 / 5) + 32;
 
         // Si no coincide con ninguna regla, devolvemos el mismo valor
         else
@@ -43,3 +78,4 @@ public class Conversor<T extends Number> {
     }    
 
 }
+
