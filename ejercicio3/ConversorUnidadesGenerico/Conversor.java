e6b917cae8b450c2b087b1a471f20c420a92e399
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-03-23 17:41:28 -0500

    Clase Conversor: Inicio de Metodo Convertir

---
+++ ejercicio3/ConversorUnidadesGenerico/Conversor.java
@@ -0,0 +1,40 @@
+package ejercicio3.ConversorUnidadesGenerico;
+
+public class Conversor<T extends Number> {
+    @SuppressWarnings({"unchecked", "UnnecessaryBoxing"})
+    // Método de conversión
+    public T convertir(Unidad<T> unidad, String unidadDestino) {
+        String unidadOrigen = unidad.getTipoUnidad().toLowerCase();
+        double valorNumerico = unidad.getValor().doubleValue(); //Momentaneamente cambiamos a tipo double para hacer las operaciones
+        double resultado;
+
+        // Longitud
+        if (unidadOrigen.equals("metros") && unidadDestino.equals("pies"))
+            resultado = valorNumerico * 3.28084;
+        else if (unidadOrigen.equals("pies") && unidadDestino.equals("metros"))
+            resultado = valorNumerico / 3.28084;
+        else if (unidadOrigen.equals("pies") && unidadDestino.equals("pulgadas"))
+            resultado = valorNumerico * 12;
+        else if (unidadOrigen.equals("pulgadas") && unidadDestino.equals("pies"))
+            resultado = valorNumerico / 12;
+        else if (unidadOrigen.equals("metros") && unidadDestino.equals("pulgadas"))
+            resultado = valorNumerico * 39.3701;
+        else if (unidadOrigen.equals("pulgadas") && unidadDestino.equals("metros"))
+            resultado = valorNumerico / 39.3701;
+
+        // Masa
+        else if (unidadOrigen.equals("kilogramos") && unidadDestino.equals("libras"))
+            resultado = valorNumerico * 2.20462;
+        else if (unidadOrigen.equals("libras") && unidadDestino.equals("kilogramos"))
+            resultado = valorNumerico / 2.20462;
+
+
+        // Si no coincide con ninguna regla, devolvemos el mismo valor
+        else
+            resultado = valorNumerico;
+        
+        return (T) Double.valueOf(resultado); //Regresamos a tipo <T>
+    }
+
+
+}
