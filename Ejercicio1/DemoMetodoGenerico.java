82e4f278614b6c5b02d5be1fec480175152b3a0b
Janitza Tapia <janitzat4@gmail.com>
2025-03-25 12:17:45 -0500

    Ejercicio1

--- Ejercicio1/DemoMetodoGenerico.java
+++ Ejercicio1/DemoMetodoGenerico.java
@@ -15,8 +15,11 @@ class DemoMetodoGenerico {
         Integer nums3[] = {1, 2, 7, 4, 5};
         Integer nums4[] = {1, 2, 7, 4, 5, 6};
 
-        if (igualArrays(nums, nums2)) System.out.println("nums es igual a nums2");
-        if (igualArrays(nums, nums3)) System.out.println("nums es igual a nums3");
-        if (igualArrays(nums, nums4)) System.out.println("nums es igual a nums4");
+        if (igualArrays(nums, nums2)) 
+            System.out.println("nums es igual a nums2");
+        if (igualArrays(nums, nums3)) 
+            System.out.println("nums es igual a nums3");
+        if (igualArrays(nums, nums4)) 
+            System.out.println("nums es igual a nums4");
     }
 }
--- Ejercicio1/TestGen.java
+++ Ejercicio1/TestGen.java
@@ -12,21 +12,21 @@ public class TestGen {
 
     public static void main(String[] args) {
         String[] v = {"Perez", "Sanchez", "Rodriguez"};
-        System.out.println(exist(v, "Sanchez")); // true
+        System.out.println(exist(v, "Sanchez") ? "Elemento encontrado: Sanchez" : "Elemento no encontrado: Sanchez");
 
         Integer[] w = {12, 34, 56};
-        System.out.println(exist(w, 34)); // true
+        System.out.println(exist(w, 34) ? "Número encontrado: 34" : "Número no encontrado: 34");
 
         Golosina g1 = new Golosina("chicle", 0.1);
         Golosina g2 = new Golosina("caramelo", 0.2);
         Golosina[] golosinas = {g1, g2};
 
-        System.out.println(exist(golosinas, new Golosina("caramelo", 0.2))); // true
+        System.out.println(exist(golosinas, new Golosina("caramelo", 0.2)) ? "Golosina encontrada: caramelo" : "Golosina no encontrada: caramelo");
 
         Chocolatina c1 = new Chocolatina("Milka");
         Chocolatina c2 = new Chocolatina("Ferrero");
         Chocolatina[] chocolates = {c1, c2};
 
-        System.out.println(exist(chocolates, c2)); // true
+        System.out.println(exist(chocolates, c2) ? "Chocolatina encontrada: Ferrero" : "Chocolatina no encontrada: Ferrero");
     }
 }
